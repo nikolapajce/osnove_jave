f4524d23741a238b2169967bfd7af62c64a3cd5d
Nikola Pavlovic <nikolapibigiz@gmail.com>
2022-05-13 19:59:42 +0200

    Typo fix

--- osnove_jave/src/main/java/p13_05_2022/Zadatak1.java
+++ osnove_jave/src/main/java/p13_05_2022/Zadatak1.java
@@ -2,6 +2,6 @@ package p13_05_2022;
 
 public class Zadatak1 {
     public static void main(String[] args) {
-        System.out.println("Hello brold");
+        System.out.println("Hello World");
     }
 }
